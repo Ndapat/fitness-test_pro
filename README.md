9dff71c12c20d27eb0d1009c2a88ac52c832a81d
Patience Joseph <patiencejosephndani@gmail.com>
2023-08-02 03:14:16 -1100

    a picture gallery of wildlife animals

---
+++ README.md
@@ -0,0 +1,1 @@
+fitness test pro class
---
+++ js/script.js
@@ -0,0 +1,72 @@
+const change = document.getElementById("image1")
+change.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change2 = document.getElementById("image2")
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change3 = document.getElementById("image3")
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change4 = document.getElementById("image4")
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change5 = document.getElementById("image5")
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change6 = document.getElementById("image6")
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change7 = document.getElementById("image7")
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change8 = document.getElementById("image8")
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change9 = document.getElementById("image9")
+change9.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change10 = document.getElementById("image10")
+change10.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change11 = document.getElementById("image11")
+change11.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
+const change12 = document.getElementById("image12")
+change12.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "yellow";
+})
+
